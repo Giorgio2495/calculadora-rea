7ce45a81fc49c975be619cfe4d8b011dd75adb55
Giovani Mesa <giovaniitj@gmail.com>
2023-01-25 17:32:13 -0500

    Add Botons +-*/

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import '../stylesheets/Boton.css'
 
 const Boton = (props) => {
+
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
   return (
-    <div>
-        {props.children}
+    <div
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+      {props.children}
     </div>
   )
 }
